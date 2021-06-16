8634b7c9a45d9215418e4632784334920cdf2bc6
github username <andydelvalle89@gmail.com>
2021-06-16 12:32:26 -0700

    add eslint watch

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 //library that allows to specify color of output displaying to console
 import chalk from "chalk";
 
-console.log(chalk.green("Starting app in dev mode..."));
+console.log(chalk.green("Starting app in dev mode...")); // eslint-disable-line no-console
